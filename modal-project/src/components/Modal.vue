9d6bdb46878df02f752b9ed733ff09ea50d65ae4
Aaron Hope <aghoecode@gmail.com>
2022-03-13 23:21:19 -0700

    lesson-28 (slots)

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <h1>{{ header }}</h1>
-      <p>{{ text }}</p>
+      <slot>default content (if no slot passed in)</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  props: ['header', 'text', 'theme'],
+  props: ['theme'],
   methods: {
     closeModal() {
       this.$emit('close')
     }
   }
 }
-
 </script>
 
-<style scoped>
+<style>
   .modal {
     width: 400px;
     padding: 20px;
-    margin: 100px auto;
+    margin: 200px auto;
     background: white;
     border-radius: 10px;
   }
@@ -35,7 +35,7 @@ export default {
     width: 100%;
     height: 100%;
   }
-  h1 {
+  .modal h1 {
     color: #03cfb4;
     border: none;
     padding: 0;
@@ -43,12 +43,33 @@ export default {
   .modal p {
     font-style: normal;
   }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+
+  /* sale styles */
   .modal.sale {
     background: crimson;
     color: white;
   }
-
   .modal.sale h1 {
     color: white;
   }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
+
 </style>
