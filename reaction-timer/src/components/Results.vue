affe45d9276a4064994db873ba5110ed6e574f3c
Aaron Hope <aghoecode@gmail.com>
2022-03-21 22:40:23 -0700

    lesson-36 (Challenge - Showing a Results Component)

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,3 +1,25 @@
 <template>
-  
+  <div class="results">
+    Reaction Time: {{ score }}ms
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['score']
+}
+
+</script>
+
+<style>
+.results {
+    width: 400px;
+    border-radius: 20px;
+    background:  #af0f9a;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+
+</style>
