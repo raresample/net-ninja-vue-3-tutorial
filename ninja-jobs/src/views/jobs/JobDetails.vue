eeb73dcb12d4060bb1cedf673eb47cc6d36e3e1a
Aaron Hope <aghopecode@gmail.com>
2022-03-26 01:36:44 -0700

    lesson-55 (Fetching Data from Components)

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,16 +1,24 @@
 <template>
-  <h1>Job Details Page</h1>
+  <h1>{{ job.title }}</h1>
   <p>The job ID is {{ id }}</p>
+  <p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
   props: ['id'],
-  // data() {
-  //   return {
-  //     id: this.$route.params.id,
-  //   }
-  // }
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+    console.log('fetched')
+  }
 }
 </script>
 
