e6656e8a0cf6400cd6c1c83b286f34b84c5fdb5b
Aaron Hope <aghopecode@gmail.com>
2022-03-27 20:03:01 -0700

    lesson-58 (VID 10 - Composition API)

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    home
+    <p>My name is {{ name }}.  And my age is {{ age }}.</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  setup() {
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me :)')
+    }
+
+    return { name, age, handleClick }
+  }
+}
+</script>
