3b4dddab6dc60cadcf9985f887b6ec03c5e6e841
Aaron Hope <aghopecode@gmail.com>
2022-03-27 20:24:33 -0700

    lesson-59 (Template Refs)

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }}.  And my age is {{ age }}.</p>
+    <p ref="p">My name is {{ name }}.  And my age is {{ age }}.</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 // @ is an alias to /src
 
 export default {
   name: 'HomeView',
   setup() {
+    const p = ref(null)
+    console.log(p.value)
+
     let name = 'mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me :)')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   }
 }
 </script>
