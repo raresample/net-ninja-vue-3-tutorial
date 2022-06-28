1a7bcbdd5e806b66a33c8cd050e6fba27c0decb1
Aaron Hope <aghopecode@gmail.com>
2022-06-27 23:53:34 -0700

    lesson-173 (/src/firebase/config.js)

---
+++ reading-list/src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDx89X-kcfTr8dAi-5o2RyZ5z84gef4dBI",
+  authDomain: "fir-9-reading-list-nn.firebaseapp.com",
+  projectId: "fir-9-reading-list-nn",
+  storageBucket: "fir-9-reading-list-nn.appspot.com",
+  messagingSenderId: "311411981942",
+  appId: "1:311411981942:web:21935577c6c583de016957"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+export { db }
