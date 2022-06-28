3bdc51036732e6bc1a0c31427b9abdf9fa53a45d
Aaron Hope <aghopecode@gmail.com>
2022-06-27 21:02:52 -0700

    lesson-103 (Signup and Login forms)

---
+++ live-chat/src/components/LoginForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+
+export default {
+  setup() {
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log(email.value, password.value)
+    }
+
+    return { email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
+</style>
---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,23 @@
+import initializeApp from 'firebase/app'
+import { getFirestore, serverTimestamp } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDybIMT4OWc04kVrHfmm2GGaPj2sSYCGzY",
+  authDomain: "net-ninja-vue-chat-app.firebaseapp.com",
+  projectId: "net-ninja-vue-chat-app",
+  storageBucket: "net-ninja-vue-chat-app.appspot.com",
+  messagingSenderId: "1087386429890",
+  appId: "1:1087386429890:web:1d36462f8f071365354e5b"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+const projectFirestore = getFirestore()
+
+console.log('firestore initialized config.js')
+console.log('db initialized config.js', db)
+
+const timestamp = serverTimestamp
+
+export { projectFirestore, timestamp }
