72cb825356fd8e423235e0d82e1e22bb181b4347
Aaron Hope <aghopecode@gmail.com>
2022-06-28 19:43:14 -0700

    lesson-179 (settting up firebase auth)

--- reading-list/src/firebase/config.js
+++ reading-list/src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDx89X-kcfTr8dAi-5o2RyZ5z84gef4dBI",
@@ -15,5 +16,6 @@ initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
+const auth = getAuth()
 
-export { db }
+export { db, auth }
